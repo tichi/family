d611515a8ebca184ee6c479e796ba9dbe3d9aa14
Tichi <github.com@tichi.org>
2012-04-11 10:56:41 -0700

    Added the SignIn view model for the Home controller, and its related resources.

---
+++ family.tichi.org/Models/View/HomeViewModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace family.tichi.org.Models.View
+{
+    /**
+     * \author Katharine Gillis
+     * \date   4/11/2012
+     * 
+     * View model for the HomeController SignIn action
+     */
+    public class HomeSignInViewModel
+    {
+        [Required(ErrorMessageResourceType = typeof(ModelRes.HomeValidationStrings), ErrorMessageResourceName = "Required")]
+        [Display(Name = "SignInEmail", ResourceType = typeof(ModelRes.HomeNameStrings))]
+        public string Email { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(ModelRes.HomeValidationStrings), ErrorMessageResourceName = "Required")]
+        [Display(Name = "SignInPassword", ResourceType = typeof(ModelRes.HomeNameStrings))]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
