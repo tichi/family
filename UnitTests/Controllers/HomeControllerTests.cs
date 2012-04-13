2c41be773e9a9c344b60bc51dc031d82899cc07e
Tichi <github.com@tichi.org>
2012-04-12 17:18:19 -0700

    Unit tests for home controller started.

---
+++ UnitTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+using NUnit.Framework;
+
+using family.tichi.org.Controllers;
+using family.tichi.org.Models.View;
+
+namespace UnitTests
+{
+    /**
+     * \author Katharine Gillis
+     * \date   4/12/2012
+     * 
+     * Unit tests for the HomeController class.
+     */
+    public class HomeControllerTests
+    {
+        /**
+         * Defines the unit test that runs the Index method, expecting the Index view to be returned.
+         */
+        [Test]
+        public void Index_Get_ReturnsIndexView()
+        {
+            // Arrange
+            HomeController testSubject = new HomeController();
+
+            // Act
+            ActionResult result = testSubject.Index();
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+
+            ViewResult viewResult = result as ViewResult;
+            Assert.That(viewResult.ViewName, Is.EqualTo("Index"));
+        }
+
+        /**
+         * Defines the unit test that runs the Index method with an invalid model, expecting the Index view to be returned with errors.
+         */
+        [Test]
+        public void Index_PostWithInvalidModelState_ReturnsIndexViewWithModelErrors()
+        {
+            // Arrange
+            HomeController testSubject = new HomeController();
+            testSubject.ModelState.Clear();
+            testSubject.ModelState.AddModelError("Email", "Email is required.");
+            
+            // Act
+            ActionResult result = testSubject.Index(new HomeSignInViewModel());
+
+            // Assert
+            Assert.That(result, Is.InstanceOf(typeof(ViewResult)));
+
+            ViewResult viewResult = result as ViewResult;
+            Assert.That(viewResult.ViewName, Is.EqualTo("Index"));
+            Assert.That(viewResult.ViewData.ModelState.IsValid, Is.False);
+        }
+
+        [Test]
+        public void Index_PostWithInvalidEmailValidPassword_ReturnsIndexViewWithModelErrors()
+        {
+        }
+    }
+}
--- family.tichi.org/Controllers/HomeController.cs
+++ family.tichi.org/Controllers/HomeController.cs
@@ -4,17 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using family.tichi.org.Models.View;
+
 namespace family.tichi.org.Controllers
 {
+    /**
+     * \author Katharine Gillis
+     * \date   4/12/2012
+     * 
+     * Actions available to the Home controller.
+     */
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
+        /**
+         * Index page.
+         * 
+         * GET: /Home/
+         */
         public ActionResult Index()
         {
             return View();
         }
 
+        /**
+         * Index page (login form submission).
+         * 
+         * POST: /Home/
+         */
+        public ActionResult Index(HomeSignInViewModel model)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
